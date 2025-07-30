239f2f2e930aad9f7aa166301bac1e282998df3c
Riadul Islam <riadcore@email.com>
2025-07-30 21:22:12 +0600

    Deploy client to GitHub for Vercel

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   baseURL: 'https://book-store-pro-4e4e.onrender.com',
+  withCredentials: true, // optional if using cookies or CORS with credentials
 });
 
 export default API;
-
